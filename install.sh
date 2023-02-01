a52153d076d4c1304e9f0c249b5885873ae7f33e
nicogarcia0 <98590527+nicogarcia0@users.noreply.github.com>
2023-02-01 19:48:39 +0100

    Create install.sh

---
+++ install.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+git clone https://github.com/trustedsec/social-engineer-toolkit
+
+	apt install python3-pip 
+		
+        cd social-engineer-toolkit
+
+        pip3 install requirements.txt
+
+        python3 setup.py
+
+        setoolkit
